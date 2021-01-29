3f5256c974644a4af973d6adde69498a4d2c072c
anky <iqoptionankit@gmail.com>
2021-01-29 13:14:22 +0530

    updating

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+/*
 let anyNum = 5;
 anyNum++;
 
 console.log(anyNum);
+*/
+
+/*
+const timeFuncRuntime = funcParameter => {
+   let t1 = Date.now();
+   funcParameter();
+   let t2 = Date.now();
+   return t2 - t1;
+}
+
+let myName = console.log("Ankit");
+
+console.log(timeFunctionRuntime(myName));
+*/
